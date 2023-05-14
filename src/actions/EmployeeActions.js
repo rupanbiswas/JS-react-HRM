12f293a6ab961fd021d22f1b60f732e77c97d9c0
rupan <rupanbiswas18rb@gmail.com>
2023-05-15 01:29:33 +0530

    hrm

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,80 +1,111 @@
-import axios from 'axios'
-import { EMPLOYEE_LOGIN_FAIL, EMPLOYEE_LOGIN_REQUEST, EMPLOYEE_LOGIN_SUCCESS, EMPLOYEE_LOGOUT, EMPLOYEE_REGISTER_FAIL, EMPLOYEE_REGISTER_REQUEST, EMPLOYEE_REGISTER_SUCCESS, EMPLOYEE_VERIFICATION_FAIL, EMPLOYEE_VERIFICATION_REQUEST, EMPLOYEE_VERIFICATION_SUCCESS } from '../constants/hrApp'
+import axios from "axios";
+import {
+  EMPLOYEE_LOGIN_FAIL,
+  EMPLOYEE_LOGIN_REQUEST,
+  EMPLOYEE_LOGIN_SUCCESS,
+  EMPLOYEE_LOGOUT,
+  EMPLOYEE_REGISTER_FAIL,
+  EMPLOYEE_REGISTER_REQUEST,
+  EMPLOYEE_REGISTER_SUCCESS,
+  EMPLOYEE_VERIFICATION_FAIL,
+  EMPLOYEE_VERIFICATION_REQUEST,
+  EMPLOYEE_VERIFICATION_SUCCESS,
+} from "../constants/hrApp";
 
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: EMPLOYEE_LOGIN_REQUEST,
+    });
+    const config = {
+      headers: {
+        "content-type": "application/json",
+      },
+    };
+    const { data } = await axios.post(
+      `${process.env.PROXY}employee/login-employee`,
+      { email, password },
+      config
+    );
+    dispatch({
+      type: EMPLOYEE_LOGIN_SUCCESS,
+      payload: data,
+    });
+    localStorage.setItem("employeeInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: EMPLOYEE_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
-export const login = (email, password)=> async (dispatch)=>{
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("employeeInfo");
+  dispatch({ type: EMPLOYEE_LOGOUT });
+};
+
+export const employeeVerificationAction =
+  (token, email) => async (dispatch) => {
     try {
-        dispatch({
-            type:EMPLOYEE_LOGIN_REQUEST
-        })
-        const config= {
-            headers:{
-                'content-type': 'application/json'
-            },
-        }
-        const {data} = await axios.post('employee/login-employee',{email,password},config)
-        dispatch({
-            type: EMPLOYEE_LOGIN_SUCCESS,
-            payload: data
-        })
-        localStorage.setItem('employeeInfo',JSON.stringify(data))
+      dispatch({
+        type: EMPLOYEE_VERIFICATION_REQUEST,
+      });
+      const config = {
+        headers: {
+          "content-type": "application/json",
+        },
+      };
+      const { data } = await axios.get(
+        `${process.env.PROXY}employee/invite-employee?token=${token}&email=${email}`,
+        config
+      );
+      dispatch({
+        type: EMPLOYEE_VERIFICATION_SUCCESS,
+        payload: data,
+      });
     } catch (error) {
-        dispatch({
-            type: EMPLOYEE_LOGIN_FAIL,
-            payload:error.response && error.response.data.message ? error.response.data.message : error.message
-        })
+      dispatch({
+        type: EMPLOYEE_VERIFICATION_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
     }
-}
+  };
 
-export const logout =()=>(dispatch)=>{
-    localStorage.removeItem('employeeInfo')
-    dispatch({type: EMPLOYEE_LOGOUT})
-}
-
-
-export const employeeVerificationAction =(token,email)=>async(dispatch)=>{
+export const EmployeeRegisterAction =
+  (token, email, password, gender, phone_number, address, country) =>
+  async (dispatch) => {
     try {
-        dispatch({
-            type:EMPLOYEE_VERIFICATION_REQUEST
-        })
-        const config= {
-            headers:{
-                'content-type': 'application/json'
-            },
-        }
-        const {data} = await axios.get(`employee/invite-employee?token=${token}&email=${email}`,config)
-        dispatch({
-            type: EMPLOYEE_VERIFICATION_SUCCESS,
-            payload: data
-        })
+      dispatch({
+        type: EMPLOYEE_REGISTER_REQUEST,
+      });
+      const config = {
+        headers: {
+          "content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        `${process.env.PROXY}employee/register-employee`,
+        { token, email, password, gender, phone_number, address, country },
+        config
+      );
+      dispatch({
+        type: EMPLOYEE_REGISTER_SUCCESS,
+        payload: data,
+      });
     } catch (error) {
-        dispatch({
-            type: EMPLOYEE_VERIFICATION_FAIL,
-            payload:error.response && error.response.data.message ? error.response.data.message : error.message
-        })
+      dispatch({
+        type: EMPLOYEE_REGISTER_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
     }
-
-}
-
-export const EmployeeRegisterAction = (token,email,password,gender,phone_number,address,country)=> async (dispatch)=>{
-    try {
-        dispatch({
-            type:EMPLOYEE_REGISTER_REQUEST
-        })
-        const config= {
-            headers:{
-                'content-type': 'application/json'
-            },
-        }
-        const {data} = await axios.post('employee/register-employee',{token,email,password,gender,phone_number,address,country},config)
-        dispatch({
-            type: EMPLOYEE_REGISTER_SUCCESS,
-            payload: data
-        })
-    } catch (error) {
-        dispatch({
-            type: EMPLOYEE_REGISTER_FAIL,
-            payload:error.response && error.response.data.message ? error.response.data.message : error.message
-        })
-    }
-}
+  };
--- src/actions/HrActions.js
+++ src/actions/HrActions.js
@@ -1,105 +1,172 @@
-import axios from 'axios'
-import { HR_LOGIN_FAIL, HR_LOGIN_REQUEST, HR_LOGIN_SUCCESS, HR_LOGOUT, INVITE_FAIL, INVITE_REQUEST, INVITE_SUCCESS, REGISTER_FAIL, REGISTER_REQUEST, REGISTER_SUCCESS, VERIFICATION_FAIL, VERIFICATION_REQUEST, VERIFICATION_SUCCESS } from '../constants/hrApp'
+import axios from "axios";
+import {
+  HR_LOGIN_FAIL,
+  HR_LOGIN_REQUEST,
+  HR_LOGIN_SUCCESS,
+  HR_LOGOUT,
+  INVITE_FAIL,
+  INVITE_REQUEST,
+  INVITE_SUCCESS,
+  REGISTER_FAIL,
+  REGISTER_REQUEST,
+  REGISTER_SUCCESS,
+  VERIFICATION_FAIL,
+  VERIFICATION_REQUEST,
+  VERIFICATION_SUCCESS,
+} from "../constants/hrApp";
 
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: HR_LOGIN_REQUEST,
+    });
+    const config = {
+      headers: {
+        "content-type": "application/json",
+      },
+    };
+    const { data } = await axios.post(
+      `${process.env.PROXY}hr/login`,
+      { email, password },
+      config
+    );
+    dispatch({
+      type: HR_LOGIN_SUCCESS,
+      payload: data,
+    });
+    localStorage.setItem("hrInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: HR_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
-export const login = (email, password)=> async (dispatch)=>{
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("hrInfo");
+  dispatch({ type: HR_LOGOUT });
+};
+
+export const registerAction =
+  (
+    first_name,
+    last_name,
+    password,
+    email,
+    gender,
+    phone_number,
+    organisation_name,
+    websiteURL,
+    address,
+    country,
+    number_of_employees
+  ) =>
+  async (dispatch) => {
     try {
-        dispatch({
-            type:HR_LOGIN_REQUEST
-        })
-        const config= {
-            headers:{
-                'content-type': 'application/json'
-            },
-        }
-        const {data} = await axios.post('hr/login',{email,password},config)
-        dispatch({
-            type: HR_LOGIN_SUCCESS,
-            payload: data
-        })
-        localStorage.setItem('hrInfo',JSON.stringify(data))
+      dispatch({
+        type: REGISTER_REQUEST,
+      });
+      const config = {
+        headers: {
+          "content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        `${process.env.PROXY}hr/register`,
+        {
+          first_name,
+          last_name,
+          password,
+          email,
+          gender,
+          phone_number,
+          organisation_name,
+          websiteURL,
+          address,
+          country,
+          number_of_employees,
+        },
+        config
+      );
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: data,
+      });
     } catch (error) {
-        dispatch({
-            type: HR_LOGIN_FAIL,
-            payload:error.response && error.response.data.message ? error.response.data.message : error.message
-        })
+      dispatch({
+        type: REGISTER_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
     }
-}
+  };
 
-export const logout =()=>(dispatch)=>{
-    localStorage.removeItem('hrInfo')
-    dispatch({type: HR_LOGOUT})
+export const verify = (token, email) => async (dispatch) => {
+  try {
+    dispatch({
+      type: VERIFICATION_REQUEST,
+    });
+    const config = {
+      headers: {
+        "content-type": "application/json",
+      },
+    };
+    const { data } = await axios.put(
+      `${process.env.PROXY}hr/register`,
+      { token, email },
+      config
+    );
+    dispatch({
+      type: VERIFICATION_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: VERIFICATION_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
-}
-
-
-export const registerAction = (first_name,last_name,password,email,gender,phone_number,organisation_name,websiteURL,address,country,number_of_employees)=> async (dispatch)=>{
-    try {
-        dispatch({
-            type:REGISTER_REQUEST
-        })
-        const config= {
-            headers:{
-                'content-type': 'application/json'
-            },
-        }
-        const {data} = await axios.post('hr/register',{first_name,last_name,password,email,gender,phone_number,organisation_name,websiteURL,address,country,number_of_employees},config)
-        dispatch({
-            type: REGISTER_SUCCESS,
-            payload: data
-        })
-    } catch (error) {
-        dispatch({
-            type: REGISTER_FAIL,
-            payload:error.response && error.response.data.message ? error.response.data.message : error.message
-        })
-    }
-}
-
-export const verify = (token,email)=> async (dispatch)=>{
-    try {
-        dispatch({
-            type:VERIFICATION_REQUEST
-        })
-        const config= {
-            headers:{
-                'content-type': 'application/json'
-            },
-        }
-        const {data} = await axios.put('hr/register',{token,email},config)
-        dispatch({
-            type: VERIFICATION_SUCCESS,
-            payload: data
-        })
-    } catch (error) {
-        dispatch({
-            type: VERIFICATION_FAIL,
-            payload:error.response && error.response.data.message ? error.response.data.message : error.message
-        })
-    }
-}
-
-export const invite = (datas)=> async (dispatch,getState)=>{
-    try {
-        dispatch({
-            type:INVITE_REQUEST
-        })
-        const {hrLogin:{hrInfo}}=getState()
-        const config= {
-            headers:{
-                'content-type': 'application/json',
-                Authorization: `Bearer ${hrInfo.token}`
-            },
-        }
-        const {data} = await axios.post('employee/invite-employee',{datas},config)
-        dispatch({
-            type: INVITE_SUCCESS,
-            payload: data
-        })
-    } catch (error) {
-        dispatch({
-            type: INVITE_FAIL,
-            payload:error.response && error.response.data.message ? error.response.data.message : error.message
-        })
-    }
-}
+export const invite = (datas) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: INVITE_REQUEST,
+    });
+    const {
+      hrLogin: { hrInfo },
+    } = getState();
+    const config = {
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${hrInfo.token}`,
+      },
+    };
+    const { data } = await axios.post(
+      `${process.env.PROXY}employee/invite-employee`,
+      { datas },
+      config
+    );
+    dispatch({
+      type: INVITE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: INVITE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
